03c97f6234c7cbfd87120ba84cf66619fb08f045
Tarso Jabbes <tarsojabbes@gmail.com>
2022-02-14 19:42:54 -0300

    Altera CSS das páginas de Login e Cadastro

--- pages/cadastro.tsx
+++ pages/cadastro.tsx
@@ -7,25 +7,34 @@ import creuza from '/assets/creuza-dos-data-science.svg';
 const Cadastro: NextPage = () => {
   return (
     <div className="bg-[#E5E5E5] h-[100vh] flex justify-between items-center">
-      <div className="flex h-[100vh] w-[50%] flex items-center justify-center">
+      <div className="h-[100vh] w-[50%] flex items-center justify-center">
 
         <main className="bg-[#FFF] flex flex-col justify-around items-start rounded-3xl gap-4 max-w-lg p-10 shadow-lg">
-          <h1 className="text-[40px] text-[#7E2ADB]">Crie sua conta <b>Educare</b></h1>
+          <h1 className="text-[40px] text-[#7E2ADB]">Crie sua conta <span className="font-black">Educare</span></h1>
           <form action="" className="flex flex-col justify-center items-start w-full gap-y-2 mb-2">
             <label className="text-[#7E2ADB]">Nome</label>
-            <input type="text" className="w-full h-8 border-solid border-2 border-[#7E2ADB] rounded-md mb-2 px-2" />
+            <input type="text" className="w-full h-8 border-solid border-2 border-[#7E2ADB] rounded-[10px] mb-2 px-2 focus:outline-none" />
             <label className="text-[#7E2ADB]">E-mail</label>
-            <input type="email" className="w-full h-8 border-solid border-2 border-[#7E2ADB] rounded-md mb-2 px-2" />
+            <input type="email" className="w-full h-8 border-solid border-2 border-[#7E2ADB] rounded-[10px] mb-2 px-2 focus:outline-none" />
             <label className="text-[#7E2ADB]">Senha</label>
-            <input type="password" className="w-full h-8 border-solid border-2 border-[#7E2ADB] rounded-md mb-2 px-2" />
-            <label className="text-[#7E2ADB]">Curso</label>
-            <input type="text" className="w-full h-8 border-solid border-2 border-[#7E2ADB] rounded-md mb-2 px-2" />
+            <input type="password" className="w-full h-8 border-solid border-2 border-[#7E2ADB] rounded-[10px] mb-2 px-2 focus:outline-none" />
           </form>
+          <fieldset className="w-[100%] inline-flex flex-row justify-evenly items-center">
+                    <div>
+                        <input type="radio" name="user-type"/>
+                        <label htmlFor="student" className="text-[#7E2ADB]"> Sou aluno</label>
+                    </div>
+                    <div>
+                        <input className="form-radio" type="radio" name="user-type"/>
+                        <label htmlFor="teacher" className="text-[#7E2ADB]"> Sou professor</label>
+                    </div>
+
+                </fieldset>
           <div className="flex justify-between items-center w-full">
 
             <span className="text-[#646464]">Já possui uma conta? <a className="underline text-[#7E2ADB] cursor-pointer">Entrar</a></span>
-            <button className="bg-[#7E2ADB] text-white text-[20px] rounded-3xl w-[180px] h-[50px] shadow-lg">
-              <Link href="/cadastro">
+            <button className="bg-[#7E2ADB] text-white text-[20px] rounded-[20px] w-[180px] h-[50px] shadow-lg">
+              <Link href="/#">
                 Cadastre-se
               </Link>
             </button>
@@ -34,12 +43,7 @@ const Cadastro: NextPage = () => {
       </div>
 
       <aside className="max-w-[50vw] pl-10 pt-10 w-[50%] h-[100vh] rounded-l-[40px] bg-cover bg-[url('../assets/waves-bg.png')] bg-no-repeat">
-        <Link href="/" >
-          <span className="text-[#fff] text-[45px] font-black cursor-pointer">Educare</span>
-        </Link>
-        <div className="flex w-full h-[80vh] items-end justify-end">
-          <Image src={creuza} width="500px" height="500px" />
-        </div>
+       
       </aside>
     </div>
 
--- pages/login.tsx
+++ pages/login.tsx
@@ -7,21 +7,21 @@ import board from '/assets/scrum-board.svg';
 const Login: NextPage = () => {
   return (
     <div className="bg-[#E5E5E5] h-[100vh] flex justify-between items-center flex-row-reverse">
-      <div className="flex h-[100vh] w-[50%] flex items-center justify-center">
+      <div className="h-[100vh] w-[50%] flex items-center justify-center">
 
-        <main className="bg-[#FFF] flex flex-col justify-around w-[35vw] items-start rounded-3xl gap-4 max-w-lg pt-20 pb-20 p-10 shadow-lg">
-          <h1 className="text-[40px] text-[#7E2ADB] mb-6">Entre com sua conta <b>Educare</b></h1>
+        <main className="bg-[#FFF] flex flex-col justify-around w-[38vw] items-start rounded-3xl gap-4 max-w-lg pt-20 pb-20 p-10 shadow-lg">
+          <h1 className="text-[40px] text-[#7E2ADB] mb-6">Entre com sua conta <span className="font-black">Educare</span></h1>
           <form action="" className="flex flex-col justify-center items-start w-full gap-y-2 mb-2">
             <label className="text-[#7E2ADB]">E-mail</label>
-            <input type="email" className="w-full h-8 border-solid border-2 border-[#7E2ADB] rounded-md mb-2 px-2" />
+            <input type="email" className="w-full h-8 border-solid border-2 border-[#7E2ADB] rounded-[10px] mb-2 px-2 focus:outline-none" />
             <label className="text-[#7E2ADB]">Senha</label>
-            <input type="password" className="w-full h-8 border-solid border-2 border-[#7E2ADB] rounded-md mb-2 px-2" />
+            <input type="password" className="w-full h-8 border-solid border-2 border-[#7E2ADB] rounded-[10px] mb-2 px-2 focus:outline-none" />
           </form>
           <div className="flex justify-between items-center w-full">
 
-            <span className="text-[#646464] underline cursor-pointer">Esqueceu a senha?</span>
-            <button className="bg-[#7E2ADB] text-white text-[20px] rounded-3xl w-[150px] h-[50px] shadow-lg">
-              <Link href="/cadastro">
+          <span className="text-[#646464]">Não possui uma conta? <br/><a className="underline text-[#7E2ADB] cursor-pointer">Cadastre-se</a></span>
+            <button className="bg-[#7E2ADB] text-white text-[20px] rounded-[20px] w-[150px] h-[50px] shadow-lg">
+              <Link href="/#">
                 Entrar
               </Link>
             </button>
@@ -30,12 +30,7 @@ const Login: NextPage = () => {
       </div>
 
       <aside className="max-w-[50vw] pl-10 pt-10 w-[50%] h-[100vh] rounded-r-[40px] bg-cover bg-[url('../assets/waves-bg.png')] bg-no-repeat">
-        <Link href="/" >
-          <span className="text-[#fff] text-[45px] font-black cursor-pointer">Educare</span>
-        </Link>
-        <div className="flex w-full h-[70vh] items-center justify-end pr-1">
-          <Image src={board} width="600px" height="400px" />
-        </div>
+        
       </aside>
     </div>
 
