c8fe06af7ce293da90844f63ca6eaa1b08b42ad2
Tarso Jabbes <tarsojabbes@gmail.com>
2022-02-15 21:31:21 -0300

    Soluciona bug do pad. bottom na página principal

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import Image1 from '/assets/principal-main-page.png'
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div >
       <nav className="w-full flex justify-between items-center pl-8 lg:pl-10 pr-10 pt-5 pb-5 absolute">
         <h1 className="text-[40px] text-[#7E2ADB] font-black">Educare</h1>
         <div className="hidden md:block">
@@ -23,11 +23,11 @@ const Home: NextPage = () => {
         </div>
       </nav>
 
-      <div className='w-full h-screen flex flex-col-reverse justify-center items-center lg:flex-row pl-8 pr-8 xl:pl-32 xl:pr-32 lg:pl-16 lg:pr-16'>
-        <div className="lg:h-screen h-fit lg:w-[50%] w-[100%] flex flex-col justify-center items-start">
+      <div className='w-full h-screen flex flex-col-reverse justify-center items-center lg:flex-row pl-8 pr-8 xl:pl-32 xl:pr-32 lg:pl-16 lg:pr-16 pt-16'>
+        <div className="lg:h-screen h-fit lg:w-[50%] w-[100%] flex flex-col justify-center items-start pb-28">
           <h1 className="font-bold text-[34px] sm:text-[44px] md:text-[55px] text-[#7E2ADB] w-[100%] sm:w-[70%] md:w-[70%] lg:w-[500px] pb-4">Lorem ipsum sit dolor amet.</h1>
           <h2 className="text-[14px] sm:text-[16px] md:text-[18px] lg:text-[20px] text-[#646464] w-[100%] sm:w-[70%] md:w-[50%] lg:w-[400px] pb-5">Lorem ipsum sit dolor amet lux elementum et laoreet.</h2>
-          <button className="bg-[#7E2ADB] text-white ext-[14px] sm:text-[16px] md:text-[18px] lg:text-[20px] rounded-[12px] md:rounded-[16px] lg:rounded-[19px] w-[200px] md:w-[230px] h-[40px] md:h-[50px] shadow-lg">
+          <button className="bg-[#7E2ADB] text-white text-[14px] sm:text-[16px] md:text-[18px] lg:text-[20px] rounded-[12px] md:rounded-[16px] lg:rounded-[19px] w-[200px] md:w-[230px] h-[40px] md:h-[50px] shadow-lg">
           <Link href="/cadastro">
             Quero aprender
           </Link>
