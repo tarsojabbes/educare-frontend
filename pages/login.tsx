a555e69dfe467d399861872be426dac7f96abf8e
Tarso Jabbes <tarsojabbes@gmail.com>
2022-02-16 13:37:58 -0300

    Adiciona responsividade à página Login

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,37 +1,59 @@
 import type { NextPage } from "next";
 import Link from "next/link";
-import Image from "next/image";
-import board from '/assets/scrum-board.svg';
-
 
 const Login: NextPage = () => {
   return (
-    <div className="bg-[#E5E5E5] h-[100vh] flex justify-between items-center flex-row-reverse">
-      <div className="h-[100vh] w-[50%] flex items-center justify-center">
-
-        <main className="bg-[#FFF] flex flex-col justify-around w-[38vw] items-start rounded-3xl gap-4 max-w-lg pt-20 pb-20 p-10 shadow-lg">
-          <h1 className="text-[40px] text-[#7E2ADB] mb-6">Entre com sua conta <span className="font-black">Educare</span></h1>
-          <form action="" className="flex flex-col justify-center items-start w-full gap-y-2 mb-2">
-            <label className="text-[#7E2ADB]">E-mail</label>
-            <input type="email" className="w-full h-8 border-solid border-2 border-[#7E2ADB] rounded-[10px] mb-2 px-2 focus:outline-none" />
-            <label className="text-[#7E2ADB]">Senha</label>
-            <input type="password" className="w-full h-8 border-solid border-2 border-[#7E2ADB] rounded-[10px] mb-2 px-2 focus:outline-none" />
-          </form>
-          <div className="flex justify-between items-center w-full">
-
-          <span className="text-[#646464]">Não possui uma conta? <br/><a className="underline text-[#7E2ADB] cursor-pointer">Cadastre-se</a></span>
-            <button className="bg-[#7E2ADB] text-white text-[20px] rounded-[20px] w-[150px] h-[50px] shadow-lg">
-              <Link href="/#">
-                Entrar
-              </Link>
-            </button>
-          </div>
+    <div className="h-[100vh]
+      flex flex-col md:flex-row justify-between items-center
+      bg-[#E5E5E5]">
+        
+      <div className="h-[100vh] w-[50%] md:w-[75%] lg:w-[50%] 
+      flex flex-col items-center justify-end md:flex-row-reverse md:justify-center md:absolute md:right-0
+      z-50">
+        <main className="h-[90%] md:h-fit w-[90vw] sm:w-[80vw] md:w-[60vw] lg:w-[38vw] pt-20 pb-20 p-10
+              flex flex-col justify-center items-center
+              bg-[#FFF] rounded-t-3xl md:rounded-3xl shadow-lg">
+                <h1 className="text-[30px] md:text-[36px] lg:text-[40px] text-[#7E2ADB]">
+                  Entre com sua conta <span className="font-black">Educare</span>
+                </h1>
+                <form className="w-full flex flex-col justify-center items-start pt-4 pb-4">
+                  <label className="text-[#7E2ADB]">
+                    E-mail
+                  </label>
+                  <input type="email" className="w-full mb-2 px-2 py-2
+                  rounded-[12px] md:rounded-[16px] lg:rounded-[16px]
+                  border-solid border-2 border-[#7E2ADB] focus:outline-none" />
+                  <label className="text-[#7E2ADB]">
+                    Senha
+                  </label>
+                  <input type="password" className="w-full mb-2 px-2 py-2
+                  border-solid border-2 border-[#7E2ADB] focus:outline-none
+                  rounded-[12px] md:rounded-[16px] lg:rounded-[16px]" />
+                </form>
+                <div className="w-full flex justify-between items-center">
+                  <span className="text-[#646464]">
+                    Não possui uma conta? <br/>
+                    <a className="underline text-[#7E2ADB] cursor-pointer">
+                      Cadastre-se
+                    </a>
+                  </span>
+                  <button className="h-[40px] md:h-[50px] w-[150px] md:w-[170px]
+                  text-white text-[14px] sm:text-[16px] md:text-[18px] lg:text-[20px]
+                  rounded-[12px] md:rounded-[16px] lg:rounded-[20px] 
+                  bg-[#7E2ADB]shadow-lg">
+                    <Link href="/#">
+                      Entrar
+                    </Link>
+                  </button>
+                </div>
         </main>
       </div>
 
-      <aside className="max-w-[50vw] pl-10 pt-10 w-[50%] h-[100vh] rounded-r-[40px] bg-cover bg-[url('../assets/waves-bg.png')] bg-no-repeat">
-        
-      </aside>
+      <aside className="h-screen md:h-screen w-[100%] md:w-[25%] lg:w-[50%] pl-10 pt-10 
+        md:block absolute
+        md:rounded-r-[40px] 
+        bg-cover bg-[url('../assets/waves-bg.png')] bg-no-repeat">
+        </aside>
     </div>
 
   )
